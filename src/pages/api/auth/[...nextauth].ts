6281d64cff3c902064c7a782e6ab8e4bf2d007e1
Jean Morillo <morillo30@gmail.com>
2023-04-02 01:18:47 -0400

    Adds social login to wenjems options

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const options = {
+export const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
