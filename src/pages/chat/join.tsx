a750c671740782c9d6b6a0debd1d9d8d62aa8fce
Jean Morillo <morillo30@gmail.com>
2023-05-03 21:07:59 -0400

    Adds loading skeleton to chat

--- src/pages/chat/join.tsx
+++ src/pages/chat/join.tsx
@@ -30,7 +30,7 @@ export default function ChatRoom() {
   const [message, setMessage] = useState("");
   const [showModal, setShowModal] = useState(false);
 
-  async function onWelcome(data: MessageData, error: string): Promise<any> {
+  async function parseMessageResponse(): Promise<MessageData[]> {
     //Getting the welcome message from the backend
     const response = await getOnlineMessages();
 
@@ -46,8 +46,16 @@ export default function ChatRoom() {
         userId: message.attributes.users_permissions_user.data.id,
       };
     });
-    console.log(data);
-    setMessages([data, ...onlineMessage]); //Storing the Welcome Message
+    return onlineMessage;
+  }
+
+  async function onWelcome(data: MessageData, error: string): Promise<any> {
+    if (data) {
+      const onlineMessage = await parseMessageResponse();
+      setMessages(onlineMessage); //Storing the Welcome Message
+    } else {
+      console.log("an error has occurred!");
+    }
   }
 
   function onMessage(data: MessageData, error: string): any {
@@ -55,8 +63,10 @@ export default function ChatRoom() {
     setMessages((prev) => [data, ...prev]);
   }
 
-  function onConnect() {
+  async function onConnect() {
     setIsConnected(true);
+    const onlineMessage = await parseMessageResponse();
+    setMessages(onlineMessage); //Storing the Welcome Message
   }
   function onDisconnect() {
     setIsConnected(false);
@@ -142,45 +152,56 @@ export default function ChatRoom() {
   return (
     <div className=" h-[calc(100vh-56px)] md:h-[100vh] w-full max-w-full overflow-hidden flex flex-col items-center">
       <div className="flex flex-col-reverse max-w-[700px] w-full h-full overflow-auto px-3 pb-4">
-        {messages.map((item, index) => {
-          const isCurrentUser = item.userId === session?.id;
-          if (item?.user?.name === "bot") {
+        {messages.length ? (
+          messages.map((item, index) => {
+            const isCurrentUser = item.userId === session?.id;
+            if (item?.user?.name === "bot") {
+              return (
+                <div
+                  key={index + "message-user"}
+                  className={`flex flex-col items-center`}
+                >
+                  <span
+                    className={`flex flex-col bg-slate-700 text-white px-4 py-2 w-fit rounded-lg my-2 shadow-md`}
+                  >
+                    <span className=" font-semibold text-sm">WebJems</span>
+                    {item.message}
+                  </span>
+                </div>
+              );
+            }
             return (
               <div
                 key={index + "message-user"}
-                className={`flex flex-col items-center`}
+                className={`${
+                  isCurrentUser && "ml-auto items-end"
+                } flex flex-col`}
               >
                 <span
-                  className={`flex flex-col bg-slate-700 text-white px-4 py-2 w-fit rounded-lg my-2 shadow-md`}
+                  className={` ${
+                    isCurrentUser
+                      ? "bg-teal-500 items-end rounded-br-none"
+                      : " bg-green-500 items-start rounded-bl-none"
+                  } flex flex-col text-white px-4 py-2 w-fit rounded-lg my-2 shadow-md`}
                 >
-                  <span className=" font-semibold text-sm">WebJems</span>
+                  <span className=" font-semibold text-sm">
+                    {!isCurrentUser && item.user?.name}
+                  </span>
                   {item.message}
                 </span>
               </div>
             );
-          }
-          return (
-            <div
-              key={index + "message-user"}
-              className={`${
-                isCurrentUser && "ml-auto items-end"
-              } flex flex-col`}
-            >
-              <span
-                className={` ${
-                  isCurrentUser
-                    ? "bg-teal-500 items-end rounded-br-none"
-                    : " bg-green-500 items-start rounded-bl-none"
-                } flex flex-col text-white px-4 py-2 w-fit rounded-lg my-2 shadow-md`}
-              >
-                <span className=" font-semibold text-sm">
-                  {!isCurrentUser && item.user?.name}
-                </span>
-                {item.message}
-              </span>
-            </div>
-          );
-        })}
+          })
+        ) : (
+          <ul className="mt-5 space-y-3 animate-pulse">
+            <li className="w-36 h-11 ml-auto bg-gray-200 rounded-md dark:bg-gray-700"></li>
+            <li className="w-36 h-11 bg-gray-200 rounded-md dark:bg-gray-700"></li>
+            <li className="w-72 h-11 bg-gray-200 rounded-md dark:bg-gray-700"></li>
+            <li className="w-36 h-11 ml-auto bg-gray-200 rounded-md dark:bg-gray-700"></li>
+            <li className="w-36 h-11 bg-gray-200 rounded-md dark:bg-gray-700"></li>
+            <li className="w-56 h-11 ml-auto bg-gray-200 rounded-md dark:bg-gray-700"></li>
+          </ul>
+        )}
       </div>
       <div className=" h-[86px] box-border flex justify-center p-5 bg-white border-t-2 border-slate-200 w-full">
         <input
