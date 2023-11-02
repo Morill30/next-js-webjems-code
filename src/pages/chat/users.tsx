ece8be39aa7d540e49f561b9e73abf515b595615
Jean Morillo <morillo30@gmail.com>
2023-11-02 00:09:53 -0400

    adds username inside button

--- src/pages/chat/users.tsx
+++ src/pages/chat/users.tsx
@@ -83,7 +83,7 @@ export default function Users() {
                     href={`/chat/user/${user.id}`}
                     className=" relative block text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-5 mb-2"
                   >
-                    Chat With
+                    Chat with {user.username}
                   </Link>
                 </div>
               </div>
