80923de506c8f434d29e239546ebc293ec4ec1c1
Jean Morillo <morillo30@gmail.com>
2023-11-05 01:51:47 -0400

    moves some things and creates edit page correctly

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -2,12 +2,23 @@ import HeaderEdit from "@/components/Profile/HeaderEdit";
 import SummaryCard from "@/components/Profile/Cards/SummaryCard";
 import SkillsCard from "@/components/Profile/Cards/SkillsCard";
 import "react-multi-carousel/lib/styles.css";
+import { SessionData } from "../api/auth/[...nextauth]";
+import { useSession } from "next-auth/react";
 
-export default function Home() {
+export default function UserProfile() {
+  const { data: session, status }: SessionData = useSession();
   return (
     <>
       <main className={`relative flex flex-col px-4`}>
         <HeaderEdit />
+        {session?.user && (
+          <a
+            className=" bg-blue-500 w-fit p-2 rounded-lg text-white"
+            href="/user/edit-profile/"
+          >
+            Edit Profle
+          </a>
+        )}
         <div
           id="profile-content"
           className="px-2 md:px-16 flex flex-wrap gap-8 py-6 md:py-10"
