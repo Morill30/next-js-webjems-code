13bd0c223704e47958fd60ffef95026ee08232be
Jean Morillo <morillo30@gmail.com>
2023-11-06 19:34:57 -0400

    adds header feature to single chats

--- src/pages/chat/user/[id].tsx
+++ src/pages/chat/user/[id].tsx
@@ -4,9 +4,19 @@ import { SessionData } from "@/pages/api/auth/[...nextauth]";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import SignInModal from "@/components/signInModal";
+import axios from "axios";
 
 type ReactInputEvent = React.ChangeEvent<HTMLInputElement> & {
   key: any;
+};
+
+type UserInfo = {
+  username?: string;
+  email?: string;
+  id?: number;
+  provider?: string;
+  displayName?: string;
+  profileImage?: string;
 };
 
 export default function ChatRoom() {
@@ -17,6 +27,15 @@ export default function ChatRoom() {
   const [inputMessage, setInputMessage] = useState("");
   const [messagesLoaded, setMessagesLoaded] = useState<boolean>(false);
   const [showModal, setShowModal] = useState(false);
+  const [friendInfo, setFriendInfo] = useState<UserInfo>({});
+
+  useEffect(() => {
+    axios.get(`/api/user/${router.query.id}`).then((response) => {
+      if (response.data) {
+        setFriendInfo(response.data?.data);
+      }
+    });
+  }, [router.query.id]);
 
   async function parseMessageResponse(): Promise<MessageData[]> {
     //Getting the welcome message from the backend
@@ -151,8 +170,20 @@ export default function ChatRoom() {
   };
 
   return (
-    <div className=" h-[calc(100vh-56px)] md:h-[100vh] w-full max-w-full overflow-hidden flex flex-col items-center">
-      <div className="flex flex-col-reverse max-w-[700px] w-full h-full overflow-auto px-3 pb-4">
+    <div className=" bg-white h-[calc(100vh-56px)] md:h-[100vh] w-full max-w-full overflow-hidden flex flex-col items-center">
+      <div className=" flex items-center gap-4 w-full bg-slate-200 p-4">
+        <div className=" flex justify-center items-center rounded-full h-9 w-9 bg-green-600 text-white uppercase">
+          {friendInfo.displayName
+            ? friendInfo.displayName[0]
+            : friendInfo?.username?.[0]}
+        </div>
+        <div className=" inline-block text-slate-500">
+          {friendInfo.displayName
+            ? friendInfo.displayName
+            : friendInfo?.username}
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col-reverse max-w-[700px] w-full h-full overflow-auto px-3 pb-4">
         {messages.length ? (
           messages.map((item, index) => {
             const isCurrentUser = item.userId === session?.id;
@@ -206,9 +237,9 @@ export default function ChatRoom() {
           <div className="flex justify-center">Chat has not started yet!</div>
         )}
       </div>
-      <div className=" h-[86px] box-border flex justify-center p-5 bg-white border-t-2 border-slate-200 w-full">
+      <div className=" h-[86px] box-border flex justify-center p-5 shadow-inner w-full">
         <input
-          className=" rounded-lg w-full max-w-[400px]"
+          className=" rounded-lg w-full max-w-[400px] bg-slate-100 border-2 border-slate-200"
           type="text"
           placeholder="Type your message"
           value={inputMessage}
@@ -219,18 +250,22 @@ export default function ChatRoom() {
         <button
           className={` ${
             !isConnected && " bg-slate-400"
-          } bg-blue-600 py-2 px-4 ml-2 rounded-lg text-white`}
+          } bg-slate-500 py-2 px-4 ml-2 rounded-lg text-white`}
           disabled={!isConnected}
           onClick={handleClick}
         >
           Send
         </button>
+        <div className=" flex items-center ml-5">
+          {isConnected ? (
+            <span className=" block h-3 w-3 bg-green-600 rounded-full shadow-md" />
+          ) : (
+            <span className=" block h-3 w-3 bg-red-600 rounded-full shadow-md" />
+          )}
+        </div>
       </div>
-      <span className=" text-center pb-2">
-        {!isConnected && "disconneted!"}
-      </span>
-      <span className=" text-center pb-2">
-        Webjems Powered chat v0.0.1 beta
+      <span className=" text-center pb-2 text-slate-800 font-thin">
+        Webjems Powered chat v0.0.2
       </span>
       <SignInModal showModal={showModal} setShowModal={setShowModalRedirect} />
     </div>
