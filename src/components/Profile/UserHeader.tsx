8a53207a348e6b970257cb0c948f5cd7afee70d0
Jean Morillo <morillo30@gmail.com>
2023-11-07 23:48:43 -0400

    references actual user instead of user that it is logged in

--- src/components/Profile/UserHeader.tsx
+++ src/components/Profile/UserHeader.tsx
@@ -3,9 +3,27 @@ import TopBackgroundHeader from "@/components/TopBackgroundHeader";
 import Icons from "@/styles/icons/Icons";
 import ImageModal from "@/components/Modals/ImageModal";
 import { useUserContext } from "@/contexts/userContext";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
 
 export default function ProfileHeader() {
+  const [currentUser, setCurrentUser] = useState<any>(null);
   const { user } = useUserContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.id) {
+      axios
+        .get(`/api/user/${router.query.id}?populate=displayName,profileImage`)
+        .then((response) => {
+          setCurrentUser(response.data.data);
+          console.log(response.data.data);
+        });
+    }
+  }, [router.query.id]);
+
   return (
     <header>
       <TopBackgroundHeader />
@@ -13,15 +31,15 @@ export default function ProfileHeader() {
         <div className="flex flex-row items-end">
           <ImageModal
             imageURL={
-              user.strapiUser?.profileImage?.url ||
+              currentUser?.profileImage?.url ||
               `${process.env.NEXT_PUBLIC_CMS_IMAGES}/profile_icon_c984ca876e.png`
             }
           >
             <div className="max-[377px]:w-20 max-[377px]:h-20 bg-white w-32 h-32 md:w-56 md:h-56 relative rounded-full border-4 border-white shadow-md flex-shrink-0">
-              {user.strapiUser?.profileImage?.url ? (
+              {currentUser?.profileImage?.url ? (
                 <Image
                   className="red object-cover w-5 rounded-full cursor-pointer"
-                  src={user.strapiUser?.profileImage?.url}
+                  src={currentUser?.profileImage?.url}
                   fill
                   style={{ objectFit: "cover" }}
                   alt="Background image"
@@ -39,30 +57,38 @@ export default function ProfileHeader() {
           </ImageModal>
           <div className="flex flex-col ml-4 md:ml-6 items-start bottom-2 md:bottom-10 relative">
             <span className="font-bold max-[377px]:text-sm text-md md:text-[20px] text-gray-700">
-              {user.strapiUser?.displayName}
+              {currentUser?.displayName}
             </span>
             <span className="font-normal max-[377px]:text-[10px] text-[12px] md:text-[14px] text-gray-500">
               This is the small description of your position
             </span>
           </div>
         </div>
-        <div className="flex items-end md:ml-auto relative pt-7 md:pt-0 pl-2 sm:pl-36 md:pl-0 md:bottom-10 ">
+        <div className="flex items-end gap-2 md:ml-auto relative pt-7 md:pt-0 pl-2 sm:pl-36 md:pl-0 md:bottom-10 ">
           <a
             type="button"
             href="https://github.com/Morill30"
             target="_blank"
-            className="text-white bg-white border-[#4285F4] border-2 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-3 py-1.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#4285F4]/30 mr-2 mb-2"
+            className="text-white bg-white border-[#4285F4] border-2 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-3 py-1.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#4285F4]/30"
           >
             <Icons type="Github" className="text-[#4285F4] w-6 h-6" />
           </a>
           <a
             type="button"
             href="mailto:morillo30@gmail.com"
-            className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+            className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55"
           >
             <Icons type="Google-Flat" />
             Send me a message
           </a>
+          {user.authUser && (
+            <Link
+              className=" bg-slate-500 hover:bg-slate-400  w-fit p-2 rounded-lg text-white text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55"
+              href="/user/edit-profile/"
+            >
+              Edit Profle
+            </Link>
+          )}
         </div>
       </div>
     </header>
--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -2,24 +2,13 @@ import UserHeader from "@/components/Profile/UserHeader";
 import SummaryCard from "@/components/Profile/Cards/SummaryCard";
 import SkillsCard from "@/components/Profile/Cards/SkillsCard";
 import "react-multi-carousel/lib/styles.css";
-import { SessionData } from "../api/auth/[...nextauth]";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
 
 export default function UserProfile() {
-  const { data: session, status }: SessionData = useSession();
   return (
     <>
       <main className={`relative flex flex-col px-4`}>
         <UserHeader />
-        {session?.user && (
-          <Link
-            className=" bg-blue-500 w-fit p-2 rounded-lg text-white"
-            href="/user/edit-profile/"
-          >
-            Edit Profle
-          </Link>
-        )}
+
         <div
           id="profile-content"
           className="px-2 md:px-16 flex flex-wrap gap-8 py-6 md:py-10"
