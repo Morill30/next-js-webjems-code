9521100a88cd05e576652826fe812df9d2f1cd82
Jean Morillo <morillo30@gmail.com>
2023-11-15 20:48:07 -0400

    Adds button back to mobile

--- src/components/Chat/ChatRoom.tsx
+++ src/components/Chat/ChatRoom.tsx
@@ -266,7 +266,7 @@ export default function ChatRoom({
         <button
           className={` ${
             !isConnected && " bg-slate-400"
-          } bg-slate-500 py-2 px-4 ml-2 rounded-lg text-white hidden md:block`}
+          } bg-slate-500 py-2 px-4 ml-2 rounded-lg text-white`}
           disabled={!isConnected}
           onClick={sendMessage}
         >
